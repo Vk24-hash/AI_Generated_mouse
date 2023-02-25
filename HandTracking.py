31085d40c1a1b72e409c441861356b900ef17d08
Vardhan <vkaashikar@gmail.com>
2023-02-25 14:03:27 +0530

    First half of the project

--- HandTracking.py
+++ HandTracking.py
@@ -5,17 +5,35 @@ import time
 cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
-hands = mpHands.Hands()
+hands = mpHands.Hands(static_image_mode=True, max_num_hands=2, min_detection_confidence=0.3)
 mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
 
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
-
-    if results.mutli_hand_landmarks:
+    # print(results.multi_hand_landmarks)
+    if results.multi_hand_landmarks:
         for handLMS in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLMS)
+            for id, lm, in enumerate(handLMS.landmark):
+                print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)
+
+                if id == 0:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+            mpDraw.draw_landmarks(img, handLMS, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3
+                , (255, 0, 255), 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
